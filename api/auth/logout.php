9aed5ac27a0f3e727e8f6248e46ba2cebc848733
Zhong86 <billy3rlvin@gmail.com>
2026-03-06 09:15:32 +0700

    Reset to Springboot & React based app

--- api/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); 
-session_destroy(); 
-
-header('Content-Type: application/json');
-echo json_encode(['success' => 200, 'message' => 'Logged out']);
-?>
--- components/footer.php
+++
@@ -1,5 +0,0 @@
-<!-- FOOTER -->
-<div class='box header d-flex justify-content-center p-3'>
-  <p style='font-size:1rem;'>Your personal library collection</p>
-</div> 
-
